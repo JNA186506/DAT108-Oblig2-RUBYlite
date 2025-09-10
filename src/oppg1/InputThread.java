7c5cf936be4525b39cb13a4bb00bc2280c729943
JohannesNA <186506@stud.hvl.no>
2025-09-10 12:57:07 +0200

    Første forslag til Oppg1

---
+++ src/oppg1/InputThread.java
@@ -0,0 +1,17 @@
+package oppg1;
+
+import javax.swing.*;
+
+public class InputThread extends Thread {
+
+    @Override
+    public void run() {
+        while (Main.fortsett) {
+            String input = JOptionPane.showInputDialog(null, "Skriv inn din melding",
+                    JOptionPane.CANCEL_OPTION);
+            System.out.println(input);
+
+            if (input == null || input.equalsIgnoreCase("quit")) Main.fortsett = false;
+        }
+    }
+}
--- src/oppg1/Main.java
+++ src/oppg1/Main.java
@@ -1,7 +1,32 @@
 package oppg1;
 
+import static java.lang.Thread.sleep;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    public static boolean fortsett = true;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread t1 = new Thread(() ->  {
+            while (fortsett) {
+                try {
+                    System.out.println("Hallo verden!");
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+        t1.start();
+
+        InputThread t2 = new InputThread();
+        t2.start();
+
+        t1.join();
+        t2.join();
+        System.out.println("Tråder avsluttes...");
+        System.out.println("Main avsluttes.");
+
     }
 }
