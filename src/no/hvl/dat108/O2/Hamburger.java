b1559b0a4d102dba9400ad999786da44c9d3711e
587712 <587712@stud.hvl.no>
2025-09-11 21:32:46 +0200

    Min versjon så langt

---
+++ src/no/hvl/dat108/O2/Hamburger.java
@@ -0,0 +1,19 @@
+package no.hvl.dat108.O2;
+
+public class Hamburger {
+	
+	private int nummer;
+	
+	public Hamburger(int nummer) {
+		
+		this.nummer = nummer;
+		
+	}
+	
+	public int getNummer() {
+		
+		return nummer;
+		
+	}
+	
+}
